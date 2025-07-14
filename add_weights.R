165673eeda5f9c9d60c0b3c11765cdb0f6badd6d
ElenaKeskinova <eli.keskinova@gmail.com>
2025-07-14 17:29:56 +0300

    w

--- add_weights.R
+++ add_weights.R
@@ -21,6 +21,34 @@ add_weights = function(ab,logw = F){
   set_edge_attr(lg,name = "weight",value = weights)
 }
 
+add_weights_corr = function(ab,bg = bgmot){ #correct for closeness to background
+  require(future.apply)
+  require(scales)
+  path = paste0("mixed-7graphs/",ab,"/")
+  
+  load(paste0(path,ab,"big7or.RData"))
+  load(paste0(path,ab,"big7-l.RData"))
+  load(file = paste0(path,ab,"_all_lcs.RData"))
+  
+  v_freqs = V(G)$Freq
+  names(v_freqs) = V(G)$name
+  
+  oddsP = sapply(V(G)$name,\(p){
+    p = unlist(strsplit(p,""))
+    prob = prod(sapply(1:7,\(i) bgmot[p[i],i]))
+    prob/(0.05**7)
+  })
+  v_freqs = v_freqs/oddsP
+  m = 1-min(v_freqs)
+  v_freqs = log2(v_freqs+m)
+  
+  
+  lcs = V(lg)$name
+  plan(multisession)
+  weights = weights(lg,edg_lcs,v_freqs)
+  set_edge_attr(lg,name = "weight",value = weights)
+}
+
 weights = function(lg,edg_lcs,v_freqs){
   future_sapply(E(lg),\(e){
     ee = ends(lg,e)
